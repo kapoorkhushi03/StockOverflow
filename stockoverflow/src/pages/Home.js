0c65cf32288e9d9d8ee169cb7bf2130525d2b80b
kapoorkhushi03 <khushikapoor200313@gmail.com>
2025-01-16 22:25:02 +0530

    start2

---
+++ stockoverflow/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar.js'
+import Footer from '../components/Footer.js'
+export default function Home() {
+  return (
+    <div>
+      <div><Navbar/></div>
+      <div><Footer/></div>
+    </div>
+  )
+}
