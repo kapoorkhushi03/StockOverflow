c0576f4fd29ecc97ee4f4f62c05b1aedb5e6f88c
kapoorkhushi03 <khushikapoor200313@gmail.com>
2025-01-17 18:17:19 +0530

    ok

--- stockoverflow/src/App.js
+++ stockoverflow/src/App.js
@@ -1,9 +1,23 @@
 
 import './App.css';
 import Home from './pages/Home';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+}from "react-router-dom";
+
 function App() {
   return (
-    <div><Home/></div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
