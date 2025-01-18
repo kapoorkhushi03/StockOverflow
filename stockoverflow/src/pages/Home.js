cee1f63d81831d8b711efbfc835c23f550f24a20
codeskavy <kanikaverma0510@gmail.com>
2025-01-18 16:14:14 +0530

    landing page update 1

--- stockoverflow/src/pages/Home.js
+++ stockoverflow/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer.js'
+import Breadcrumbs from '../components/BreadCrumbs.js'
+import CustomCarousel from '../components/CustomCarousel.js'
+import Faq from '../components/Faq.js'
 export default function Home() {
   return (
     <div>
       <div><Navbar/></div>
+      <div><Breadcrumbs/></div>
+      <div><Faq/></div>
+
+     <div><CustomCarousel/></div>
+
       <div><Footer/></div>
     </div>
   )
