7ed2cafb29e387b4ad29cb2b417f7acaa1d96bb9
codeskavy <kanikaverma0510@gmail.com>
2025-01-18 15:17:53 +0530

    footer

--- stockoverflow/src/components/Footer.js
+++ stockoverflow/src/components/Footer.js
@@ -1,9 +1,124 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default function footer() {
+export default function Footer() {
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer className="bg-light text-center text-lg-start text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+        <div>
+          <a href="#" className="me-4 text-reset">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <i className="fab fa-google"></i>
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="#" className="me-4 text-reset">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <Container className="text-center text-md-start mt-5">
+          <Row className="mt-3">
+            <Col md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <i className="fas fa-gem me-3"></i>Company name
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+              </p>
+            </Col>
+
+            <Col md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Angular
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  React
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Vue
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Laravel
+                </a>
+              </p>
+            </Col>
+
+            <Col md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Help
+                </a>
+              </p>
+            </Col>
+
+            <Col md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <p>
+                <i className="fas fa-home me-2"></i> New York, NY 10012, US
+              </p>
+              <p>
+                <i className="fas fa-envelope me-3"></i> info@example.com
+              </p>
+              <p>
+                <i className="fas fa-phone me-3"></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className="fas fa-print me-3"></i> + 01 234 567 89
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}
+      >
+        © 2021 Copyright: 
+        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
 }
