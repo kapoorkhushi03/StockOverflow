3fa9a04b352f16bc7bd7edcb8cc5541cfdd7602b
kapoorkhushi03 <khushikapoor200313@gmail.com>
2025-01-21 21:21:21 +0530

    login

--- stockoverflow/src/pages/Home.js
+++ stockoverflow/src/pages/Home.js
@@ -4,14 +4,9 @@ import Footer from '../components/Footer.js'
 import Breadcrumbs from '../components/BreadCrumbs.js'
 import CustomCarousel from '../components/CustomCarousel.js'
 import Faq from '../components/Faq.js'
-<<<<<<< HEAD
-=======
+import Main from '../components/Main.js'
 import Features from '../components/Features.js'
 import Take from '../components/Take.js'
-import Main from '../components/Main.js'
-
->>>>>>> 9e5f3efb1334a4b3a2cab63f15daee9b26f49ac0
-
 export default function Home() {
   return (
     <div>
