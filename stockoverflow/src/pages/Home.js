fc2e863187a329d23404a5f5cdef26b1bb6d6098
kapoorkhushi03 <khushikapoor200313@gmail.com>
2025-01-22 22:30:37 +0530

    bg

--- stockoverflow/src/pages/Home.js
+++ stockoverflow/src/pages/Home.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer.js'
-import Breadcrumbs from '../components/BreadCrumbs.js'
 import CustomCarousel from '../components/CustomCarousel.js'
 import Faq from '../components/Faq.js'
 import Main from '../components/Main.js'
 import Features from '../components/Features.js'
 import Take from '../components/Take.js'
+import '../styles/Home.css';
+
 export default function Home() {
   return (
-    <div>
+    <div className='bg'>
       <div><Navbar/></div>  
-        <div><Breadcrumbs/></div>
+       
       <div>
         <Main/>
       </div>
